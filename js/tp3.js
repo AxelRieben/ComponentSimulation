d8c5cf7eb94e135b8138d105c3c76983467fbe89
Sylvain Renaud <sylvain.renaud@he-arc.ch>
2018-03-02 14:38:41 +0100

    Add inputs for scheduling.

--- js/tp3.js
+++ js/tp3.js
@@ -1,71 +1,75 @@
 //Canavas and context
-var canvas = document.getElementById("canvas");
-var context = canvas.getContext('2d');
-var rect = canvas.getBoundingClientRect();
+let canvas = document.getElementById("canvas");
+let context = canvas.getContext('2d');
+let rect = canvas.getBoundingClientRect();
 
-init()
-rendering()
+init();
+rendering();
 
-function init(){
+function init() {
 
 }
-async function running(){
-	//clear the context
-	context.clearRect(0, 0, canvas.width, canvas.height);
-	context.strokeStyle = '#000000';
-	context.fillStyle = '#FFFFFF';
-	context.lineWidth = 1;
-	
-	context.rect(20,20,150,100);
-	context.fill();
-	context.stroke();
-	
-	context.beginPath()
-	context.arc(200,200,80,0,2*Math.PI);
-	context.fill();
-	context.stroke();
-	
-	context.beginPath();
-	context.moveTo(0,0);
-	context.lineTo(400,400);
-	context.closePath();
-	context.stroke();
-	
-	context.beginPath();
-	context.moveTo(0,400);
-	context.lineTo(400,0);
-	context.closePath();
-	context.stroke();
-	
-	context.beginPath();
-	context.moveTo(0,200);
-	context.lineTo(400,200);
-	context.closePath();
-	context.stroke();
-	
-	context.beginPath();
-	context.moveTo(200,0);
-	context.lineTo(200,400);
-	context.closePath();
-	context.stroke();
-	
-	context.beginPath();
-	context.arc(200,200,40,0,2*Math.PI);
-	context.fillStyle = '#000000';
-	context.fill();
-	context.stroke();
+
+async function running() {
+    //clear the context
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.strokeStyle = '#000000';
+    context.fillStyle = '#FFFFFF';
+    context.lineWidth = 1;
+
+    context.rect(20, 20, 150, 100);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(200, 200, 80, 0, 2 * Math.PI);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.lineTo(400, 400);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(0, 400);
+    context.lineTo(400, 0);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(0, 200);
+    context.lineTo(400, 200);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.moveTo(200, 0);
+    context.lineTo(200, 400);
+    context.closePath();
+    context.stroke();
+
+    context.beginPath();
+    context.arc(200, 200, 40, 0, 2 * Math.PI);
+    context.fillStyle = '#000000';
+    context.fill();
+    context.stroke();
 }
+
 // redraw
-function rendering() {     
+function rendering() {
     requestAnimationFrame(rendering);
-	running();
+    running();
 }
+
 //calculation if spring length with pythagore
-function springLength(X,Y){
-	return Math.sqrt(Math.pow(X,2)+Math.pow(Y,2))
+function springLength(X, Y) {
+    return Math.sqrt(Math.pow(X, 2) + Math.pow(Y, 2))
 }
+
 //update html
-function updateRangeValue(val,field){
-	 document.getElementById(field).value=val;
-	 initValues();
+function updateRangeValue(val, field) {
+    document.getElementById(field).value = val;
+    initValues();
 }
